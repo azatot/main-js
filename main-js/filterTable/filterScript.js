aadb001f5dae15c7f91abf165319776c3c08dfc1
Denys Ostrovskyi <azatot2014@gmail.com>
2019-07-13 17:09:40 +0300

    added animated arrow

--- main-js/filterTable/filterScript.js
+++ main-js/filterTable/filterScript.js
@@ -1,383 +1,250 @@
-// Task priorities:
-// 1) зробити скрипт універсальним, розбивши його на css-маніпуляції
-// вийшло створити цілий клас з конструктором який створює елементи
-// 2) створити універсальні функції, які базуються на аргументах та колбеках
-class Dos_HTML_Element {
-    constructor(html_tag, html_id, html_class, html_text) {
-        this.html_el = document.createElement(html_tag);
-        this.html_el.id = html_id;
-        this.html_el.className = html_class;
-        this.html_el.innerText = html_text;
-        this.html_el.type;
-        
-        this.wrapper;
-        
+_DosFilter = () => {
+    function Dos_HTML_Element(html_tag, html_id=null, html_class=null, html_text=null, html_html=null) {
+        let html_el = document.createElement(html_tag);
+        html_el.id = html_id;
+        html_el.className = html_class;
+        html_el.innerText = html_text;
+        html_el.innerHTML = html_html;
+        html_el.type;
+        return html_el;
+
     }
-    dos_wrap_currentNode(idValue, position) {
-        this.wrapper = document.createElement("div");
-        this.wrapper.id = idValue;
-        this.wrapper.style.cssText = `
-                display: flex;
-                flex-direction: row; 
-                margin: 0em;
-                padding: 0;
-                
-                position: ${position};
-                z-index: 100;
-                background: darkgray;
-                `;
-        this.wrapper.append(this.html_el);
-        return this.wrapper;
-    }
-    dos_css_setSize(width, height, heightUnits, display = "flex") {
-        this.html_el.style.cssText = 
-        `
-            display: ${display};
-            width: ${width}${heightUnits};
-            height: ${height}${heightUnits};
-            border: 1px solid;
+
+    const _stylesObj = {
+
+        main_wrapper: `
+        display: flex;
+        flex-direction: row;
+        margin: 1em;
+        font-size: large;
+        padding: 10px;
+        position: fixed;
+        z-index: 100;
+        font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+        
+    `,
+
+        button_Show: `
+            display: flex;
+            width: 5em;
+            height: 5em;
+            
+            
             align-items: center;
             align-self: center;
             text-align: center;
             justify-content: center;
             padding: 0.5em;
-            background-color: ;
+            transition: all 0.4s ease 0s;
+
+            border-radius: 0%;
             cursor: pointer;
+
+        `,
+        
+        button_Hide: ``,
+
+        arrow: `
+            display: flex;
+            width: 7em;
+            height: 7em;
+            background-color: rgb(130, 130, 130);
+            padding: 0px;
+            border-radius: 10px;
+            box-shadow: 0px 0px 10px 0px #515151;
+            transition: all 0.3s ease 0s;
+            z-index: 1000;
+        `,
+
+        wrapper_For_Inputs: `
+            display: none;
+            flex-direction: row;
+            margin: 0em 0em 0em 0.5em;
+            padding: 0px 1em;
+            z-index: 100;
+            opacity: 1;
+            transition: .3s;
+            border: 0px solid;
+            border-top-right-radius: 10px;
+            border-bottom-right-radius: 10px;
+            background: darkgray;
+            box-shadow: 0px 0px 8px 0px #00000070;
+
+        `,
+
+        filter_Input_Field: `
+            height: 38px;
+            align-self: center;
+            margin: 0em;
+            border: none;
+            border-radius: 5px;
+            font-size: 16px;
+            padding-left: 10px;
+            width: 150px;
+            font-weight: light;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+        `,
+
+        filter_Submit_Button: `
+            display: flex;
+            justify-content: center;
+            align-self: center;
+            width: 6em;
+            height: 2.65em;
+            margin-left: 0.5em;
+            background: none;
+            border: 1px solid;
+            cursor: pointer;
+            font-size: smaller;
+            
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            
+
         `
 
     }
 
-}
+    // creating elements
+    const firstDiv = new Dos_HTML_Element("div", "dos_firstDiv", "dos_active", "SHOW");
+    const dos_arrow = new Dos_HTML_Element("img", null, null, null);
+        dos_arrow.src = "https://unitronicsplc.com/wp-content/uploads/intense-cache/icons/plugin/font-awesome/arrow-circle-right.svg";
+    const dos_wrapper = new Dos_HTML_Element("div", "dos_wrapper", "dos_active")
+    const new_dos_wrapper = new Dos_HTML_Element("div", "dos_secondDiv", "dos_active");
+    const newInput = new Dos_HTML_Element("input");
+    newInput.placeholder = 'words to search...';
+    const submitInput = new Dos_HTML_Element("input", "dos_search_button");
+    submitInput.type = "submit";
+    submitInput.value = "FIND";
+    const clearButton = new Dos_HTML_Element("input", "dos_clear_button");
+    clearButton.type = "submit";
+    clearButton.value = "CLEAR";
+console.log(document.querySelector("i"))
+    // styling this elements
+    dos_wrapper.style.cssText = _stylesObj.main_wrapper;
+    firstDiv.style.cssText = _stylesObj.button_Show;
+    dos_arrow.style.cssText = _stylesObj.arrow;
+    new_dos_wrapper.style.cssText = _stylesObj.wrapper_For_Inputs;
+    newInput.style.cssText = _stylesObj.filter_Input_Field;
+    submitInput.style.cssText = _stylesObj.filter_Submit_Button;
+    clearButton.style.cssText = _stylesObj.filter_Submit_Button;
 
-
-    // створення елементів і задання їм місця на сторінці
-    let firstDiv = new Dos_HTML_Element("div", "dos_firstDiv", "dos_active", "SHOW"); 
-        firstDiv.dos_css_setSize(5, 5, 'em');
-    document.body.prepend(firstDiv.dos_wrap_currentNode("dos_wrapper", "fixed"));
-    // document.querySelector("#dos_firstDiv").dos_css_setSize(5, 5, "em");
-    let dos_wrapper = document.querySelector("#dos_wrapper");
-
-    let secondDiv = new Dos_HTML_Element("div", "dos_secondDiv", "dos_active", null);
-    
-
-    dos_wrapper.append(secondDiv.dos_wrap_currentNode("new_dos_wrapper", "none"));
-    let new_dos_wrapper = document.querySelector("#new_dos_wrapper");
-
-
-    // styling new_dos_wrapper
-    new_dos_wrapper.style.opacity = "0";
-    new_dos_wrapper.style.display = "none";
-    new_dos_wrapper.style.transition = "all 0.3s";
-    new_dos_wrapper.style.border = "1px solid";
-    new_dos_wrapper.style.paddingLeft = "1em";
-    new_dos_wrapper.style.paddingRight = "1em";
-    new_dos_wrapper.style.marginLeft = "0.5em";
-
-
-    // ну не створюється в мене інпут через конструктор, сука!
-    // створив поле вводу
-    let newInput = document.createElement("input");
+    // appearing those elements
+    document.body.prepend(dos_wrapper);
+    dos_wrapper.append(firstDiv);
+    firstDiv.appendChild(dos_arrow);
+    dos_wrapper.append(new_dos_wrapper);
     new_dos_wrapper.append(newInput);
-    newInput.style.height = "38px";
-    newInput.style.alignSelf = "center";
-    newInput.style.margin = "0em";
-    newInput.style.border = "none";
-    newInput.style.fontSize = "1.5em";
-    newInput.style.paddingLeft = "10px";
-    // newInput.style.paddingTop = "5px";
-    // newInput.style.paddingBottom = "5px";
-    newInput.style.width = "150px";
-    
-    newInput.style.fontWeight = "bold";
-    newInput.style.fontFamily = "Arial";
-    
-    
-    // кнопка для пошуку
-    let submitInput = document.createElement("input");
-    submitInput.type = "submit";
     new_dos_wrapper.append(submitInput);
+    new_dos_wrapper.append(clearButton)
 
-    submitInput.type = "submit";
-    submitInput.style.borderStyle = "none";
-    submitInput.style.border = "1px solid";
-    submitInput.style.background = "none";
-    submitInput.style.height = "3em";
-    submitInput.style.marginLeft = "0.5em";
-    submitInput.style.display = "flex";
-    submitInput.style.alignSelf = "center";
-    submitInput.style.cursor = "pointer";
-
-
-
-    console.log()
-    console.log(firstDiv)
-
-    let input = document.querySelectorAll("input");
+    // main logic
     let targetElements = [];
-    let vpNameList = document.querySelectorAll("td");
+    const startTransform = dos_arrow.style.transform;
+    const vpNameList = (tag) => document.querySelectorAll(tag);
 
-    function insertArgs(...args) {
+    insertArgs = (...args) => {
         let arr = [...args];
-
-        arr.forEach(function (el) {
+        arr.forEach((el) => {
             return highlightElements(el, "green")
-
         })
 
     }
-    function highlightElements(str, color) {
-        vpNameList.forEach(function (el, i) {
+
+    highlightElements = (str, color) => {
+
+        // у 126 строці аргументом вводиться потрібний для виділленяня тег
+        vpNameList("td").forEach((el, i) => {
             let lowCaseText = el.innerText.toLowerCase();
             if (lowCaseText.includes(str)) {
                 el.style.backgroundColor = color;
-                return targetElements.push(el);
+                targetElements.push(el);
             }
-            
+
+        })
+        console.log(targetElements)
+
+    }
+
+    clearHighlight = () => {
+        targetElements.forEach((el) => {
+            el.style.backgroundColor = null;
+
         })
 
     }
-    submitInput.onkeydown = function(e){
-        if(e.keyCode === 13){
-            popDownWindow();
 
-        }
-
-    }
-    submitInput.onclick = function(e){
-        popDownWindow();
- 
-    }
-    function clearHighlight(){
-        targetElements.forEach(function(el){
-            el.style.backgroundColor = "";
-        })
-    }
-    function popDownWindow() {
+    popDownWindow = () => {
         if (String(newInput.value) == "") {
             newInput.style.boxShadow = "rgba(255, 0, 0, 0.49) 0px 0px 3px 3px inset";
         } else {
             insertArgs(newInput.value);
             newInput.value = "";
-            
+
         }
 
     }
 
-
-    
-
-    (function showPopUpWindow() {
+    showPopUpWindow = () => {
         let visible = false;
-            firstDiv.html_el.onclick = function showClose() {
+        dos_wrapper.onmouseover = () => {
+            start_color = event.target.style.backgroundColor;
+            if (event.target.id === firstDiv.id) {
+                event.target.style.cssText += `
+                    background-color: #7faeff;
+                    cursor: pointer;
+
+                `;
                 
+            } 
+
+        }
+        dos_wrapper.onmouseout = () => {
+            if (event.target.id === firstDiv.id) {
+                event.target.style.backgroundColor = start_color;
+
+            }
+            
+        }
+        
+        firstDiv.onclick = showClose = () => {
+            
             if (visible) {
-                new_dos_wrapper.style.opacity = '0';
                 visible = false;
+                new_dos_wrapper.style.opacity = '0';
+                
+                if (event.target.tagName === "IMG") {
+                    event.target.style.transform = startTransform;
+                    
+                }
                 
                 setTimeout(() => {
-                    firstDiv.html_el.innerText = "SHOW"
+                    new_dos_wrapper.style.display = "none";
                     
-                    new_dos_wrapper.style.display = "none"
-                }, 199);
-               
+                }, 300);
+
             } else {
-                new_dos_wrapper.style.opacity = '1';
                 visible = true;
                 
+                if (event.target.tagName === "IMG") {
+                    event.target.style.transform = "scale(-1,1)"
+                }
+               
+                
+
                 setTimeout(() => {
-                    firstDiv.html_el.innerText = "HIDE"
-                    
                     new_dos_wrapper.style.display = "flex";
-                }, 199);
-                
-                
-                
-                
-                
+                    new_dos_wrapper.style.opacity = '1';
+                }, 300);
+
             }
-        }
-        document.querySelector("#dos_wrapper").onmouseover = function() {
-            start_color = event.target.style.backgroundColor;
-            if (event.target.id === firstDiv.html_el.id) {
-                event.target.style.cssText += `
-                    background-color: white;
-                    cursor: pointer;
-                `;
-                   
-            }
-        }
-        document.querySelector("#dos_wrapper").onmouseout = function() {
-            if (event.target.id === firstDiv.html_el.id) {
-                event.target.style.backgroundColor = start_color;
-            }
-        }
-    })();
-    
-    
-    
-    
-    
-
-    
-    // чисто потестити, як будуть працювати ф-ії подій
-    // (function testFunc(color, targetElem, start_color) {
-        
-    //     
-    //     document.querySelector("#dos_firstDiv").onclick = function() {
-            
-    //     }
-        
-    // })('darkgrey');
-
-    
-     
-    
-
-    // елемент створюється та вставляється в документ у вигляді:
-    //      <div id="dos_firstDiv" class="dos_active"> 
-    
-    
-    // задаємо елементу width і height в будь-яких одиницях виміру
-    
-
-
-/*
-
-
-function askWordsToHide() {
-    let targetElements = [];
-    
-    let openPopButton = document.querySelector(".button");
-        openPopButton.innerText = "SHOW FILTER"
-        openPopButton.style.marginBottom = "0";
-    
-    let closePopButton = document.createElement("div");
-    let blockWrapper = document.querySelector("#block-wrapper")
-    let vpNameList = document.querySelectorAll("td");
-    
-    let input = document.createElement("input");
-    let windowDiv = document.createElement("div");
-        windowDiv.style.display = "none";
-    let titleOfDiv = document.createElement("p");
-    let clearFilter = document.createElement("div");
-
-    let buttonsBlock = document.querySelector("#buttons");
-        // buttonsBlock.style.display = flex;
-        // buttonsBlock.style.flexDirection = column;
-        
-    // ф-ія, що приймає будь-яку кількість аргументів
-    // перетворює в массив
-    // перебирає масив, виконуючи для кожного ф-ію hideElements()
-
-    function insertArgs(...args) {
-        let arr = [...args];
-
-        arr.forEach(function (el) {
-            return highlightElements(el, "green")
-
-        })
-
-    }
-
-    //ф-ія, що приховує заданий строчний аргумент
-    function highlightElements(str, color) {
-        vpNameList.forEach(function (el, i) {
-            let lowCaseText = el.innerText.toLowerCase();
-            if (lowCaseText.includes(str)) {
-                el.style.backgroundColor = color;
-                return targetElements.push(el);
-            }
-            
-        })
-
-    }
-
-  
-
-
-    // Enter натискаємо - функцію запускаємо
-    input.onkeydown = function(e){
-        if(e.keyCode === 13){
-            popDownWindow();
 
         }
 
     }
+    showPopUpWindow();
 
-    // ЛКМ жмякаю - ... і popUpWindow();
-    closePopButton.onclick = function(e){
-        popDownWindow();
- 
-    }
-
-    // Відкриває вікно
-
-    function modifyWindow() {
-        
-        blockWrapper.appendChild(windowDiv);
-        // blockWrapper.insertBefore()
-        console.log(this)
-        if (windowDiv.style.display == "none") {
-            closePopButton.className = "button";
-            closePopButton.innerText = "close and apply";
-            input.placeholder = "Words for search...";
-
-            titleOfDiv.innerText = "filter".toUpperCase();       
-            titleOfDiv.className = "flex-center";
-            titleOfDiv.className = "title_of_window";
-            
-            windowDiv.style.display = "flex";
-            windowDiv.id = "showPopUpWindow";
-            windowDiv.appendChild(input);
-            buttonsBlock.appendChild(closePopButton);
-            buttonsBlock.appendChild(clearFilter);
-            buttonsBlock.style.display = "flex";
-            buttonsBlock.style.flexDirection = "column";
-            buttonsBlock.style.width = "200px";
-            windowDiv.appendChild(buttonsBlock);
-            windowDiv.insertBefore(titleOfDiv, windowDiv.firstChild);
-            
-            
-            clearFilter.innerText = "reset";
-            clearFilter.style.display = "flex";
-            clearFilter.className = "button";
-
-            this.innerText = "HIDE FILTER";
-            
-        } else  {
-            windowDiv.style.display = "none";
-            this.innerText = "SHOW FILTER";
-            
-        }
-    }
-
-    function clearHighlight(){
-        targetElements.forEach(function(el){
-            el.style.backgroundColor = "";
-        })
-    }
-
-    openPopButton.addEventListener("click", modifyWindow, false);
-    clearFilter.addEventListener("click", clearHighlight, false); 
-
-
-    // закриває вікно
-    function popDownWindow() {
-        if (String(input.value) == "") {
-            input.style.boxShadow = "rgba(255, 0, 0, 0.49) 0px 0px 0px 3px inset";
-        } else {
-            insertArgs(input.value);
-            input.value = "";
-            
-        }
-
-    }
-    
-
-
+    submitInput.addEventListener("click", popDownWindow)
+    clearButton.addEventListener("click", clearHighlight)
 }
-askWordsToHide();
-
-
-
-*/
+_DosFilter();
