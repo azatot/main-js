80f76cc30ff2e76b7a9670bafaf1ad61039f73e1
Denys Ostrovskyi <azatot2014@gmail.com>
2019-07-18 17:42:32 +0300

    test animation with js in sandbox

--- sandbox.js
+++ sandbox.js
@@ -2,23 +2,36 @@
 console.log("...Sandbox here...\n↕↕↕↕↕↕↕↕↕↕↕↕↕↕↕↕↕↕\n↕↕↕↕↕↕↕↕↕↕↕↕↕↕↕↕↕↕");
 // Test
 
-const MyObj3 = initVal => {
-    let myVal = initVal
-    return {
-        get: function () {
-            return myVal
-        },
-        set: function (val) {
-            myVal = val
-        }
+const arrowElement = document.querySelector("#show_button");
+const menuRadio = document.querySelector("#menu_radio");
+
+let isVisible = false;
+arrowElement.onclick = () => {
+    if (isVisible === false) {
+        isVisible = true;
+        menuRadio.style.left = 150 + "px";
+        menuRadio.style.width = 200 + "px";
+        console.log(menuRadio.children)
+
+        setTimeout(() => {
+            for(let el of menuRadio.children){
+                el.style.visibility = "visible";
+                el.zIndex = "2";
+            }
+        }, 200);
+    } else if(isVisible === true) {
+        isVisible = false;
+        menuRadio.style.left = 50 + "px";
+        menuRadio.style.width = 0 + "px";
+        console.log(menuRadio.children)
+        setTimeout(() => {
+            for(let el of menuRadio.children){
+                el.style.visibility = "hidden";
+                el.zIndex = "-1";
+
+            }
+        }, 200);
     }
 }
-const x = MyObj3(0);
-const y = MyObj3(4);
-console.log(y.get())
-console.log(x);
-console.log(x.get())
-
-
 
 
